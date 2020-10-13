061f5eba28c9f8fa4b4e85f4e7317c02eb57226a
Abdullahi Aden <Abbe6152@gmail.com>
2020-10-13 16:59:14 +0200

    before answer correction.

--- quiz.js
+++ quiz.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
-  fetch("https://quizapi.io/api/v1/questions?apiKey=mK0SPtcOzyhQODarlNLDzoZOUogQMh5IenMC945k&category=code&difficulty=Easy&limit=3")
+  fetch("https://quizapi.io/api/v1/questions?apiKey=mK0SPtcOzyhQODarlNLDzoZOUogQMh5IenMC945k&category=code&difficulty=Easy&limit=5")
   .then(response => response.json())
   .then(data => {
     let game = new Quiz(data);
-    console.log(data);
+    // console.log(data);
   })  
 })
 
@@ -13,12 +13,9 @@ class Quiz {
   constructor(fetchedQuestions){
     this.loadedQuestions=[]
     this.questionCounter=0;
-    this.maxQuestions=10
+    this.maxQuestions=5
 
-    this.filteredAnswerChoices=[]
-// ***going to hold the users clicked answers to be later compared to the correct answers array
-    this.selectedAnswers=[]
-
+    this.score;
 
 // *** contains an object created from the Question class which holds all the json objects from the api 
     console.log(this.loadedQuestions);
@@ -26,73 +23,142 @@ class Quiz {
     for (let fetchedQuestion of fetchedQuestions ){
       this.loadedQuestions.push(new Question(fetchedQuestion))
     }
+    console.log(this.loadedQuestions);
 
-    console.log(this.loadedQuestions[this.questionCounter].question);
+    // this.loadedQuestions[this.questionCounter].displayCurrentQuestion()
+    // this.nextButton()
     this.startQuiz()
+      // this.loadedQuestions[this.questionCounter].displayCurrentAnswers()
+
+
+    // console.log(this.loadedQuestions);
+    // this.nextButton()
     // this.nextButton()
   }
 
   startQuiz(){
+    // this.questionCounter++
+    // if(this.endQuiz){
 
-
-    this.displayQuiz()
-
-    let mainContainer = document.getElementById('mainContainer');
+    // }
+    let startBtn = document.getElementById('startBtn');
+    startBtn.addEventListener("click",e=>{
+      startBtn.classList.add("hide")
+      
+ let mainContainer = document.getElementById('mainContainer');
     mainContainer.classList.remove("hide")
 
-    
-    // this.loadedQuestions[this.questionCounter].displayCurrentAnswers()
-
-
+      this.loadedQuestions[this.questionCounter].displayCurrentQuestion()
+    })
     this.nextButton()
 
   }
   nextButton(){
-    // *** load next question each with its answers everytime its clicked
-    this.maxQuestions=0
-
-        this.questionCounter++
-        console.log(this.questionCounter);
-
-
+    
     let nextBtn = document.getElementById('nextBtn');
-    nextBtn.addEventListener("click", function(e){
+ 
+    // *** load next question each with its answers everytime its clicked
+        // this.questionCounter++
+        // console.log(this.questionCounter);
+        // console.log(this.questionCounter);
+        // console.log(this.loadedQuestions);
+    nextBtn.addEventListener("click", e=>{
+    
 
-      
+      // ************** empty the question and answers
+      let questionDisplay = document.getElementById('questionDisplay');
+      questionDisplay.innerText=""
 
-      console.log(e.target);
+      let answerContainer = document.getElementById('answerContainer');
+      answerContainer.innerText=""
+      this.questionCounter ++
+      this.loadedQuestions[this.questionCounter].displayCurrentQuestion()
 
-      // if(this.questionCounter>=this.maxQuestions){
-      //  console.log('game over');
-      // }
-      // this.endGame()
+      // this.questionCounter++
+      // console.log(e.target);
+     console.log(this.maxQuestions);
+     console.log(this.questionCounter);
+
+     
+
+      // if (this.questionCounter===this.maxQuestions){
+      //   // show score
+      //   console.log("enddd");
+      // } 
+
+      this.endQuiz()
     })
+    
+
   }
 
-  displayQuiz(){
-    // ***************************************
-    if(this.endQuiz()){
-      console.log('endddd');
-    }
-    let questionDisplay = document.getElementById('questionDisplay');
-    // *** using 
-    questionDisplay.innerText=this.loadedQuestions[this.questionCounter].question
-// gives array without null
-    console.log(this.filteredAnswerChoices);
-  
-//  WE ARE FILTERING THIS.ANSWER FROM THE QUESTION CLASS, IN ORDER TO ACCESS THAT, WE USE LOADEDQUES...  
-// WITH AN ATTRIBUTE OF THIS CLASS THEN PUSH TO AN ARRAY IN THIS CLASS
-  this.filteredAnswerChoices.push(this.loadedQuestions[this.questionCounter].answers.filter(unfilteredAnswers => unfilteredAnswers !== null))
-  console.log(this.filteredAnswerChoices.length);
+  endQuiz(){
 
-  // **** TODO: MAKE SURE TO GET THE ANSWERS IN OWN LINE???????????????????????????????????????????
-    for (let i = 0; i<this.filteredAnswerChoices.length;i++){
+     if(this.questionCounter===this.maxQuestions){
+       console.log('game over');
+      console.log(this.questionCounter); 
+      console.log(this.maxQuestions); 
+
+      let intro = document.getElementById('introContainer');
+      intro.innerText="GAME OVER"
+
+      let questionDisplay = document.getElementById('questionDisplay');
+      let answerContainer = document.getElementById('answerCOntainer');
+      questionDisplay.classList.add("hide")
+      answerContainer.classList.add("hide")
+      
+      // location.reload()
+      }
+    // show score 
+
+  }
+  
+}
+class Question{
+  constructor(question){
+      this.question =question.question
+      this.answers =Object.values (question.answers ) 
+      this.correct_answers =question.correct_answers
+
+  
+      this.selectedAnswers=[]
+
+      // console.log(this.answers);
+      // console.log(this.question);
+      // console.log(this.correct_answers);
+
+
+      // this.displayCurrentQuestion()
+  }      
+
+  displayCurrentQuestion(){
+
+    // ******** TODO: kolla om det är endQuiz, om inte kör frågor och svar 
+    // ******** problem??? calling that method in Quiz
+
+    let questionDisplay = document.getElementById('questionDisplay');
+    questionDisplay.innerText=this.question
+
+    // this.displayCurrentAnswers()
+    this.displayCurrentAnswers()
+
+  }
+
+  displayCurrentAnswers(){
+    // answers with null
+    console.log(this.answers);
+  // gives array without null
+// *** push filtered ans into arr
+  let filteredAnswerChoices =this.answers.filter(unfilteredAnswers => unfilteredAnswers !== null)
+  console.log(filteredAnswerChoices.length);
+
+    for (let i = 0; i<filteredAnswerChoices.length;i++){
       let answerContainer = document.getElementById('answerContainer');
 
       let answerCheckbox = document.createElement("input")
       answerCheckbox.type="checkbox"
       answerCheckbox.classList.add("checkbox")
-      let answerLabel=document.createTextNode(this.filteredAnswerChoices[i])
+      let answerLabel=document.createTextNode(filteredAnswerChoices[i])
       answerContainer.appendChild(answerCheckbox)
       answerContainer.appendChild(answerLabel)
 
@@ -120,30 +186,6 @@ class Quiz {
     // answerChoices.innerText=
     // this.nextButton()
   }
-
-  endQuiz(){
-    // return window.location.assign()
-
-  }
-  
-}
-class Question{
-  constructor(question){
-      this.question =question.question
-      this.answers =Object.values (question.answers ) 
-      this.correct_answers =question.correct_answers
-
-  
-
-      // console.log(this.answers);
-      // console.log(this.question);
-      // console.log(this.correct_answers);
-
-
-      // this.displayCurrentQuestion()
-  }      
-
-  
 }      
 
 
