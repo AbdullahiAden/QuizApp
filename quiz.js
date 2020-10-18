51c5e3f841ce12ca450ba6ecf382e5ce482e664a
Abdullahi Aden <Abbe6152@gmail.com>
2020-10-18 11:42:05 +0200

    final - with  score to be fixed

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
-  fetch("https://quizapi.io/api/v1/questions?apiKey=mK0SPtcOzyhQODarlNLDzoZOUogQMh5IenMC945k&category=code&difficulty=Easy&limit=5")
+  fetch("https://quizapi.io/api/v1/questions?apiKey=mK0SPtcOzyhQODarlNLDzoZOUogQMh5IenMC945k&category=code&difficulty=Easy&limit=10")
   .then(response => response.json())
   .then(data => {
     let game = new Quiz(data);
@@ -13,103 +13,159 @@ class Quiz {
   constructor(fetchedQuestions){
     this.loadedQuestions=[]
     this.questionCounter=0;
-    this.maxQuestions=5
+    this.maxQuestions=10
 
-    this.score;
-
-// *** contains an object created from the Question class which holds all the json objects from the api 
-    console.log(this.loadedQuestions);
+    this.selectedAnswers=[]
+    this.correctAnswerChoices=[]
+    this.score=0
+    console.log(fetchedQuestions);
 
     for (let fetchedQuestion of fetchedQuestions ){
       this.loadedQuestions.push(new Question(fetchedQuestion))
     }
+    // contains an object created from the Question class which holds all the json objects from the api 
     console.log(this.loadedQuestions);
-
-    // this.loadedQuestions[this.questionCounter].displayCurrentQuestion()
-    // this.nextButton()
     this.startQuiz()
-      // this.loadedQuestions[this.questionCounter].displayCurrentAnswers()
-
-
-    // console.log(this.loadedQuestions);
-    // this.nextButton()
-    // this.nextButton()
   }
 
   startQuiz(){
     // this.questionCounter++
-    // if(this.endQuiz){
-
-    // }
     let startBtn = document.getElementById('startBtn');
     startBtn.addEventListener("click",e=>{
       startBtn.classList.add("hide")
       
- let mainContainer = document.getElementById('mainContainer');
+    let mainContainer = document.getElementById('mainContainer');
+    let nextBtn = document.getElementById('nextBtn');
     mainContainer.classList.remove("hide")
+    nextBtn.classList.remove("hide")
+    let intro = document.getElementById('introContainer');
+    intro.classList.add("hide")
 
-      this.loadedQuestions[this.questionCounter].displayCurrentQuestion()
+
+      this.displayCurrentQuestion()
+        this.questionCounter++
     })
     this.nextButton()
 
   }
-  nextButton(){
-    
-    let nextBtn = document.getElementById('nextBtn');
- 
-    // *** load next question each with its answers everytime its clicked
-        // this.questionCounter++
-        // console.log(this.questionCounter);
-        // console.log(this.questionCounter);
-        // console.log(this.loadedQuestions);
-    nextBtn.addEventListener("click", e=>{
-    
+  displayCurrentQuestion(){
+    let questionDisplay = document.getElementById('questionDisplay');
+    questionDisplay.innerText=this.loadedQuestions[this.questionCounter].question
+    this.displayCurrentAnswers()
+  }
 
-      // ************** empty the question and answers
+  displayCurrentAnswers(){
+    // answers with null
+    // console.log(this.loadedQuestions[this.questionCounter].answers);
+    let toBeFiltered=Object.values( this.loadedQuestions[this.questionCounter].answers)
+    // console.log(this.correct_answers);
+  // gives array without null
+// *** push filtered ans into arr
+    let filteredAnswerChoices =toBeFiltered.filter(unfilteredAnswers => unfilteredAnswers !== null)
+  // console.log(filteredAnswerChoices.length);
+    for (let i = 0; i<filteredAnswerChoices.length;i++){
+      let answerContainer = document.getElementById('answerContainer');
+      let answerDiv = document.createElement("p")
+ 
+      answerDiv.classList.add("divAnswers")
+      answerDiv.innerText=filteredAnswerChoices[i]
+      answerContainer.appendChild(answerDiv)
+   
+      answerDiv.addEventListener("click", e =>{
+        if(answerDiv.classList.contains("clicked")){
+          answerDiv.classList.remove("clicked")
+        }else{
+            answerDiv.classList.add("clicked")
+        }
+
+      })
+      
+    }
+    this.correctTheQuestion()
+  }
+
+   correctTheQuestion(){
+    let correctAnswers =Object.values( this.loadedQuestions[this.questionCounter].correct_answers)
+    let answerChoices= this.loadedQuestions[this.questionCounter].answers
+
+    let userIndex=[]
+    console.log(correctAnswers);
+    console.log(answerChoices);
+
+    let userAnswers = Array.from(document.querySelectorAll('.divAnswers'))
+    // let checkedAns = Array.from(document.getElementsByClassName('checkbox'))
+
+    userAnswers.forEach(check=>{
+      check.addEventListener('click', e=>{
+       userIndex.push( Array.from(userAnswers).indexOf(e.target) )
+   console.log(userIndex);
+
+   let trueIndex=[ correctAnswers.indexOf("true")]
+    console.log( trueIndex);
+// *** if user clicks correct one more than once it increments the score 
+    for(var i=0;i<trueIndex.length;i++){
+      if(userIndex[i]!==trueIndex[i]){
+        console.log("score is 0");
+        return "False"; 
+
+       } else{
+
+         this.score++
+         console.log('scored 1');
+       }
+      } 
+      });
+    })
+  
+  }
+
+  nextButton(){
+  
+    let nextBtn = document.getElementById('nextBtn');
+    let questionNr = document.getElementById('questionNr');
+    let intro = document.getElementById('introContainer');
+
+    nextBtn.addEventListener("click", e=>{
+      
+       if(this.questionCounter>=this.maxQuestions){
+           
+      let replay = document.getElementById('replay');
+      replay.classList.remove("hide")
+
+       console.log("endd");
+       let nextBtn = document.getElementById('nextBtn');
+       nextBtn.classList.add("hide")
+       let questionContainer = document.getElementById('questionContainer');
+       questionContainer.classList.add("hide")
+
+       intro.classList.remove("hide")
+       intro.innerText="GAME OVER! Your Score is " + this.score
+
+       this.rePlay()
+
+    }
+      // empty the question and answers
       let questionDisplay = document.getElementById('questionDisplay');
       questionDisplay.innerText=""
 
       let answerContainer = document.getElementById('answerContainer');
       answerContainer.innerText=""
+      // increment the count to print question and answers
+      this.displayCurrentQuestion()
       this.questionCounter ++
-      this.loadedQuestions[this.questionCounter].displayCurrentQuestion()
-
-      // this.questionCounter++
-      // console.log(e.target);
-     console.log(this.maxQuestions);
-     console.log(this.questionCounter);
-
-     
-
-      // if (this.questionCounter===this.maxQuestions){
-      //   // show score
-      //   console.log("enddd");
-      // } 
-
-      this.endQuiz()
+        
+      questionNr.innerText=this.questionCounter
     })
     
 
   }
 
-  endQuiz(){
+  rePlay(){
+    let replay = document.getElementById('replay');
+    replay.addEventListener("click",e=>{
+      location.reload()
 
-     if(this.questionCounter===this.maxQuestions){
-       console.log('game over');
-      console.log(this.questionCounter); 
-      console.log(this.maxQuestions); 
-
-      let intro = document.getElementById('introContainer');
-      intro.innerText="GAME OVER"
-
-      let questionDisplay = document.getElementById('questionDisplay');
-      let answerContainer = document.getElementById('answerCOntainer');
-      questionDisplay.classList.add("hide")
-      answerContainer.classList.add("hide")
-      
-      // location.reload()
-      }
-    // show score 
+    })
 
   }
   
@@ -117,111 +173,9 @@ class Quiz {
 class Question{
   constructor(question){
       this.question =question.question
-      this.answers =Object.values (question.answers ) 
-      this.correct_answers =question.correct_answers
+      this.answers =question.answers 
+      this.correct_answers = question.correct_answers
 
-  
-      this.selectedAnswers=[]
-
-      // console.log(this.answers);
-      // console.log(this.question);
-      // console.log(this.correct_answers);
-
-
-      // this.displayCurrentQuestion()
   }      
 
-  displayCurrentQuestion(){
-
-    // ******** TODO: kolla om det är endQuiz, om inte kör frågor och svar 
-    // ******** problem??? calling that method in Quiz
-
-    let questionDisplay = document.getElementById('questionDisplay');
-    questionDisplay.innerText=this.question
-
-    // this.displayCurrentAnswers()
-    this.displayCurrentAnswers()
-
-  }
-
-  displayCurrentAnswers(){
-    // answers with null
-    console.log(this.answers);
-  // gives array without null
-// *** push filtered ans into arr
-  let filteredAnswerChoices =this.answers.filter(unfilteredAnswers => unfilteredAnswers !== null)
-  console.log(filteredAnswerChoices.length);
-
-    for (let i = 0; i<filteredAnswerChoices.length;i++){
-      let answerContainer = document.getElementById('answerContainer');
-
-      let answerCheckbox = document.createElement("input")
-      answerCheckbox.type="checkbox"
-      answerCheckbox.classList.add("checkbox")
-      let answerLabel=document.createTextNode(filteredAnswerChoices[i])
-      answerContainer.appendChild(answerCheckbox)
-      answerContainer.appendChild(answerLabel)
-
-
-      answerCheckbox.addEventListener("click", e =>{
-        if(e.target.checked){
-
-          let userChoice=this.selectedAnswers.push(e.target)
-          console.log(this.selectedAnswers);
-
-          console.log('is chekced');
-// **** TODO: get the value of the clcked checbox----------------------------
-          return userChoice
-        }
-
-        // console.log(e.target);
-
-
-        // **** give the correct answer as the one of the choices, and give it id to
-        // **** check if clicked answer is correct
-
-      })
-    }
-
-    // answerChoices.innerText=
-    // this.nextButton()
-  }
 }      
-
-
-
-
-
-// class Quiz {
-//   constructor(){
-//       this.startBtn = document.getElementById('startBtn');
-//       this.nextBtn = document.getElementById('nextBtn');
-//       this.main = document.getElementById('mainContainer');
-//       this.introContainer = document.getElementById('introContainer');
-
-      
-      
-//       this.startQuiz()
-
-      
-//   }
-// startQuiz(){
-//     startBtn.addEventListener("click",e=>{
-
-//       this.startBtn.classList.add("hide")
-//       this.nextBtn.classList.remove("hide")
-//       this.main.classList.remove("hide")
-//       this.introContainer.classList.add("hide")
-            
-//       let question = new AllQuestions;
-
-
-//           //  question.getQuestions()
-//         })
-// }
-
-// //  endQuiz()
-
-
-
-// }
