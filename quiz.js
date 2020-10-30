4cfd5678f6a0dccb11ef43d3c5dc8e94b29ec0d3
Abdullahi Aden <Abbe6152@gmail.com>
2020-10-30 22:47:15 +0100

    Done - maybe look for bugs

--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-  // https://quizapi.io/api/v1/questions?apiKey=mK0SPtcOzyhQODarlNLDzoZOUogQMh5IenMC945k&category=code&difficulty=Easy&limit=10
-  fetch("question.json")
+  //
+  // question.json 
+  fetch("https://quizapi.io/api/v1/questions?apiKey=mK0SPtcOzyhQODarlNLDzoZOUogQMh5IenMC945k&category=code&difficulty=Easy&limit=10")
     .then(response => response.json())
     .then(data => {
       let game = new Quiz(data);
@@ -58,9 +59,8 @@ class Quiz {
     // answers with null
     // console.log(this.loadedQuestions[this.questionCounter].answers);
     let toBeFiltered = Object.values(this.loadedQuestions[this.questionCounter].answers)
-    // console.log(this.correct_answers);
+    
     // gives array without null
-    // *** push filtered ans into arr
     let filteredAnswerChoices = toBeFiltered.filter(unfilteredAnswers => unfilteredAnswers !== null)
     // console.log(filteredAnswerChoices.length);
     for (let i = 0; i < filteredAnswerChoices.length; i++) {
@@ -86,53 +86,73 @@ class Quiz {
 
   correctTheQuestion() {
     let correctAnswers = Object.values(this.loadedQuestions[this.questionCounter].correct_answers)
+    let multiple_correct_answers = this.loadedQuestions[this.questionCounter].multiple_correct_answers
     let answerChoices = this.loadedQuestions[this.questionCounter].answers
 
-    let userIndex = []
+    console.log(multiple_correct_answers);
     console.log(correctAnswers);
     console.log(answerChoices);
+    let userIndex = []
+    let trueIndexes = []
 
+    // loop through the correct answers array and push the indexes that hold the keyword "true" into trueIndexes array, to be later compared with users answers
+    let i = -1;
+    while ((i = correctAnswers.indexOf("true", i + 1)) != -1) {
+      trueIndexes.push(i)
+    }
+    console.log(trueIndexes);
     let userAnswers = Array.from(document.querySelectorAll('.divAnswers'))
-    // let checkedAns = Array.from(document.getElementsByClassName('checkbox'))
 
-    // loop thorugh answer choices, get the index that was clicked, 
-    // get the index of true in corrrect answer choices, compare it with clicked, if match increment score 
+    // push the clicked div in userIndex, then compare with trueIndexes which holds the correct answer indexes, then match them
     userAnswers.forEach(check => {
       check.addEventListener('click', e => {
-        if(check.classList.contains("clicked")){
-            userIndex.push(Array.from(userAnswers).indexOf(e.target))
-        console.log(userIndex);
+        if (check.classList.contains("clicked")) {
+          userIndex.push(Array.from(userAnswers).indexOf(e.target))
+          console.log(userIndex);
 
-        let trueIndex = [correctAnswers.indexOf("true")]
-        console.log(trueIndex);
-        // *** if user clicks correct one more than once it increments the score , also increments clicking wrong one after clicking  correct one 
-        // ** also cannot get score clicking the correct one after clicked wrong the first time
-        // ** make sure to check if more than one answer can be chosen...........
-        for (var i = 0; i < trueIndex.length; i++) {
-          if (userIndex[i] !== trueIndex[i])  { 
-
-           this.score=0;
-          console.log("score is 0");
-          } else {
-
-            this.score++
-            console.log('scored 1');
-          }
+        } else {
+// means is not clicked, unclicked
+          userIndex = []
+       
         }
+ // check multiple answer
+            if (multiple_correct_answers === "true") {
+              console.log('multi true');
+                if (trueIndexes.every((val)=> userIndex.includes(val)) ){
+                this.score++;
+                console.log('multiple');
+                console.log("score is 1");
+                console.log(this.score);
 
-        } else{
-          // *** can click wrong and click right and get point 
-          userIndex= [ ]
-        }
-      
+              }
+  // check multiple that is shown "false" in multiple_correct_answers in the API data
+            } else if (multiple_correct_answers === "false" && trueIndexes.length > 1) {
+                
+              if (trueIndexes.every((val)=> userIndex.includes(val)) ){
+                this.score++
+                console.log('multi false but scored 1' );
+                console.log(this.score);
 
+              }
+            }
+ // check for single answer
+            else if (multiple_correct_answers === "false" && trueIndexes.length <= 1) {
+                  if (trueIndexes.every((val)=> userIndex.includes(val)) ){
+
+                  this.score  ++;
+                  console.log("scored1");
+                  console.log(this.score);
+                } else {
+                  // ** when chosen correct ans, and clikc with wrong, it ++
+                  // this.score++
+                  console.log('scored 0');
+                  console.log(this.score);
+                }
+
+            }
       });
 
-      
-
-
     })
-
   }
 
   nextButton() {
@@ -191,6 +211,7 @@ class Question {
     this.question = question.question
     this.answers = question.answers
     this.correct_answers = question.correct_answers
+    this.multiple_correct_answers = question.multiple_correct_answers
 
   }
 
